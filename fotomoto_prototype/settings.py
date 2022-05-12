217d95ee8d2b6fec67fdc209979d1e92d1baff45
Chad Jemmett <cjemmett@wehco.com>
2022-05-12 10:34:30 -0500

    Changed database again

--- fotomoto_prototype/settings.py
+++ fotomoto_prototype/settings.py
@@ -83,27 +83,27 @@ DEVELOPMENT_MODE = os.getenv("DEVELOPMENT_MODE", "False") == "True"
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
-
 # DATABASES = {
-#         'default': {
-#             'ENGINE': 'django.db.backends.postgresql',
-#             'NAME': 'fotomoto',
-#             'USER': "cjem",
-#             'PASSWORD': "hellolego",
-#             'HOST': os.getenv("DATABASE_URL"),
-#             'PORT': '5432',
-#             }
-#         }
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+
+DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': 'fotomoto',
+            'USER': "cjem",
+            'PASSWORD': "hellolego",
+            'HOST': os.getenv("DATABASE_URL"),
+            'PORT': '5432',
+            }
+        }
 # print("Thi sis the database host >>>>>>>>>>>>", DATABASES['default']['HOST'])
 
-# DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 # if DEVELOPMENT_MODE is True:
 #     DATABASES = {
 #     'default': {
