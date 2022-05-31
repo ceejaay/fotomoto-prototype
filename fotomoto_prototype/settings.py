9b879257e6520c2de2fb761a4ba444560efbf5b2
Chad Jemmett <cjemmett@wehco.com>
2022-05-31 14:34:16 -0500

    Making some changes to get fotomoto to work

--- fotomoto_prototype/settings.py
+++ fotomoto_prototype/settings.py
@@ -29,7 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", get_random_secret_key())
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG=False
+DEBUG=True
 # DEBUG = os.getenv("DEBUG", "False") == "True"
 ALLOWED_HOSTS = ['localhost', ".herokuapp.com"]
 CSRF_TRUSTED_ORIGINS = ["https://*.herokuapp.com"]
@@ -161,7 +161,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
